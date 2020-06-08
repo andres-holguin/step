db83ed5346ac9d08bc1dee546ef8471ac4afea9c
andres-holguin <andresdholguin98@gmail.com>
2020-06-08 22:54:01 +0000

    Store timestamp with each comment for sorting
    Comments will now be queried in ascending order by timestamp, so the most recent comments appear at the bottom of the comments list.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,6 +25,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.FetchOptions;
 
 import com.google.gson.Gson;
@@ -78,7 +79,7 @@ public class DataServlet extends HttpServlet {
     ArrayList<String> comments = new ArrayList<String>();
     if (numComments ==  null || numComments < 0) numComments = Integer.MAX_VALUE;
 
-    Query query = new Query("Comment");
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.ASCENDING);
     List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(numComments));
 
     for (Entity entity : results) {
@@ -94,6 +95,7 @@ public class DataServlet extends HttpServlet {
   private void addNewComment(String commentText) {
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("text", commentText);
+    commentEntity.setProperty("timestamp", System.currentTimeMillis());
 
     datastore.put(commentEntity);
   }
